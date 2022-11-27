125e04f4b00eeb3dd5ace261d1e87c9e44e3931b
Vikas Mishra <vikmishra@google.com>
2022-11-27 22:15:22 +0530

    Minimal configuration from a blank Doom Config.

--- packages.el
+++ packages.el
@@ -11,7 +11,7 @@
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
-;; https://github.com/raxod502/straight.el#the-recipe-format
+;; https://github.com/radian-software/straight.el#the-recipe-format
 ;(package! another-package
 ;  :recipe (:host github :repo "username/repo"))
 
@@ -34,11 +34,12 @@
 
 ;; Specify a `:branch' to install a package from a particular branch or tag.
 ;; This is required for some packages whose default branch isn't 'master' (which
-;; our package manager can't deal with; see raxod502/straight.el#279)
+;; our package manager can't deal with; see radian-software/straight.el#279)
 ;(package! builtin-package :recipe (:branch "develop"))
 
 ;; Use `:pin' to specify a particular commit to install.
 ;(package! builtin-package :pin "1a2b3c4d5e")
+
 
 ;; Doom's packages are pinned to a specific commit and updated from release to
 ;; release. The `unpin!' macro allows you to unpin single packages...
@@ -47,65 +48,3 @@
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
-;
-
-;; ---------------------------------
-;; Vikas's custom packager additions
-;; ---------------------------------
-;; Beacon mode to highlight the line
-(package! beacon)
-;; Tabnine based AI completion backend for company
-(package! company-tabnine)
-
-;; Org and Org related packages to make org mode more effective.
-;; Enable org-roam-ui, this needs the latest version of org-roam and so we need to unpin the version
-;; that doom has chosen
-(package! org-roam-ui)
-;; Consult notes
-(package! consult-notes
-  :recipe (:host github :repo "mclear-tools/consult-notes"))
-(package! consult-org-roam)
-; Use org-modern package to make the Org mode look a little modern
-(package! org-modern)
-;; Org-ql
-(package! org-ql)
-
-;; I like tree-sitter
-(package! tree-sitter)
-(package! tree-sitter-langs)
-
-;; W3m
-(package! w3m)
-
-;; all-the-icons
-(package! all-the-icons)
-
-;; Tomorrow Bright Color them - my favorite color theme
-(package! color-theme-sanityinc-tomorrow)
-
-(package! org-super-agenda)
-
-(package! pretty-hydra)
-
-;; Enable visual-fill mode
-(package! visual-fill-column)
-
-
-;; I want smooth scrolling
-(package! good-scroll)
-
-;; I need org-appear
-(package! org-appear)
-
-;; Org sidebar
-(package! org-ol-tree
-  :recipe (
-           :host github
-           :repo "Townk/org-ol-tree"
-           ))
-
-;; Improved Search
-(package! ctrlf)
-
-;; Show a preview of line to move to
-(package! goto-line-preview)
